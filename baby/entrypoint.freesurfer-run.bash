330fe93e2dad37f1ca01be2c9653ef6e62e9958a
Paul Wighton <paul@corticometrics.com>
2021-01-14 23:46:15 +0000

    [WIP] moving runtime config from dockerfile to entrypoint

--- baby/entrypoint.freesurfer-run.bash
+++ baby/entrypoint.freesurfer-run.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
+rm -f $FREESURFER_HOME/python/bin/python3
+ln -s `which python` $FREESURFER_HOME/python/bin/python3
+
+ln -s $FS_INFANT_MODEL/sscnn_skullstrip/cor_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/cor_sscnn.h5
+ln -s $FS_INFANT_MODEL/sscnn_skullstrip/ax_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/ax_sscnn.h5
+ln -s $FS_INFANT_MODEL/sscnn_skullstrip/sag_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/sag_sscnn.h5 
 
 if [ -n "${FS_KEY}" ]; then
   echo "[entrypoint.bash] -- FS_KEY detected. Creating ${FREESURFER_HOME}/license.txt."
